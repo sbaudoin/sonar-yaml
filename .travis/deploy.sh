cca9c1ab097bdc8986671e35feff471eb4cd2293
sbaudoin <sylvain.baudoin@gmail.com>
2018-07-25 22:14:30 +0200

    Added traces

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "Deploying..."
+
 # Get Maven project version
 MAVEN_VERSION=$(grep '<version>' pom.xml | head -1 | sed 's/<\/\?version>//g'| awk '{print $1}')
+echo "Maven version: $MAVEN_VERSION"
+echo "Travis branch: $TRAVIS_BRANCH"
+echo "Travis tag: $TRAVIS_TAG"
 
 # Deploy only if tag or master branch and SNAPSHOT version
 if [[ -n "$TRAVIS_TAG" || ( "$TRAVIS_BRANCH" = "master" && "$MAVEN_VERSION" =~ -SNAPSHOT$ ) ]]
