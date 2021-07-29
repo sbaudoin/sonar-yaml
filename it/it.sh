af7322626f97e30a4ba2517bec515de287343697
sbaudoin <sylvain.baudoin@gmail.com>
2021-07-29 19:20:06 +0200

    Debug

--- it/it.sh
+++ it/it.sh
@@ -19,13 +19,17 @@ docker-compose -f $SCRIPT_DIR/docker-compose.yml down
 # Start containers
 echo "Starting SonarQube..."
 docker-compose -f $SCRIPT_DIR/docker-compose.yml up -d sonarqube
+echo "---- Compose started..."
 CONTAINER_NAME=$(docker ps --format "{{.Names}}" | grep 'it_sonarqube_1.*' | head -1)
+echo "---- Container ID: $CONTAINER_NAME"
 # Wait for SonarQube to be up
 grep -q "SonarQube is up" <(docker logs --follow --tail 0 $CONTAINER_NAME)
 # Copy the plugin
 MAVEN_VERSION=$(grep '<version>' $SCRIPT_DIR/../pom.xml | head -1 | sed 's/<\/\?version>//g'| awk '{print $1}')
+echo "---- Copying plugin JAR file..."
 docker cp $SCRIPT_DIR/../target/sonar-yaml-plugin-$MAVEN_VERSION.jar $CONTAINER_NAME:/opt/sonarqube/extensions/plugins
 # Restart SonarQube
+echo "---- Restarting SonarQube..."
 docker-compose -f $SCRIPT_DIR/docker-compose.yml restart sonarqube
 # Wait for SonarQube to be up
 grep -q "SonarQube is up" <(docker logs --follow --tail 0 $CONTAINER_NAME)
