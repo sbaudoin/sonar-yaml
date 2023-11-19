94a5652d6197c6098f207418e519f60dfb845e5b
sbaudoin <sylvain.baudoin@gmail.com>
2023-11-20 00:41:36 +0100

    Add new supported rule property value

--- src/main/java/com/github/sbaudoin/sonar/plugins/yaml/checks/NewLinesCheck.java
+++ src/main/java/com/github/sbaudoin/sonar/plugins/yaml/checks/NewLinesCheck.java
@@ -23,6 +23,6 @@ import org.sonar.check.RuleProperty;
  */
 @Rule(key = "NewLinesCheck")
 public class NewLinesCheck extends YamlLintCheck {
-    @RuleProperty(key = "type", description = "UNIX-typed ('unix') or DOS-typed ('dos') new line characters", defaultValue = "unix")
+    @RuleProperty(key = "type", description = "UNIX-typed ('unix'), DOS-typed ('dos') or platform-dependent ('platform') new line characters", defaultValue = "unix")
     String type;
 }
